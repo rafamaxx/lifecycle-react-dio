c4a84a15159cc5eab61351437494a3f06e14500b
rafamaxx <rafamaxi.rm@gmail.com>
2021-06-20 03:22:26 -0300

    Hooks

--- lifecycle-advanced-react-dio/src/Twitter.js
+++ lifecycle-advanced-react-dio/src/Twitter.js
@@ -1,42 +1,39 @@
-import React, {Component} from 'react'
+import React, {useEffect, useState, memo} from 'react'
 
-class Twitter extends Component{
-
-    state={
-        tweet: 'title'
-    }
-
-    componentDidMount(){
-        const {posts, loading} = this.props
-        console.log('componentDidMount',posts)
-        console.log('componenctDidUpdat: loading', loading)
-    }
-
-    componentDidUpdate(prevProps){
-        const {loading} = this.props
-        if(this.props.loading !== prevProps.loading){
-            console.log('componentDidUpdate: loading', loading)
-        }
-    }
-
-    componentWillUnmount(){
-        console.log('componentWillUnmount: Removed :')
-    }
-
-    shouldComponentUpdate(nextProps, nextState){
-        return this.state.tweet !== nextState.tweet
-    }
-
-
-    render(){
-        const {posts} = this.props
-        console.log('Render',posts)
-        return(
-            <div>
-                Teste
-            </div>
-        )
-    }
+const areEqual = (prevProps, nextProps)=>{
+    return prevProps.loading === nextProps.loading
 }
 
-export default Twitter
+function Twitter(props) {
+    const {loading} = props
+    const [tweet,setTweet] = useState('title')
+
+    useEffect(()=>{
+        const { posts, loading } = props
+        console.log('componentDidMount', posts)
+        console.log('componenctDidUpdat: loading', loading)
+    },[])
+
+    useEffect(()=>{
+        console.log('componetDidUpdate', loading)
+    }, [loading])
+
+    useEffect(() =>{
+        return () =>{
+            console.log('componentWillUnmount: removed')
+        }
+    },[])
+
+    const handleTweet = () =>{
+        setTweet = ('Tweet Atualizado')
+    }
+
+    console.log('Tweet Atualizado', tweet)
+    return (
+        <div>
+            Teste
+        </div>
+    )
+}
+
+export default memo(Twitter,areEqual)
